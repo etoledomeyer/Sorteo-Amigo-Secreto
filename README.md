ec0823b46fa9dce2f93552f6b5b64305e3dafd41
etoledo <enggeltm@gmail.com>
2025-03-17 14:00:06 -0300

    Versión 1.0 del Proyecto

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Sorteo-Amigo-Secreto
-Desafio Alura
+<img align="center" src="logo.png" alt="Título README">
+<h2 align="left">Descripcion del Proyecto<h2>
+<p>Este es un programa que utiliza lo aprendido en la primera etapa del curso ONE - Oracle Alura Latam relacionado con Javascript y HTML</p>
+<p>El programa determina el nombre de una persona (amigo) al azar dentro de una lista ingresada, como ganador</p>
+<h2 align="left">Estado del Proyecto<h2>
+<p>Finalizado</p>
+<h2>Funcionalidades del proyecto</h2>
+<li>Ingreso de nombres (amigos)</li>
+<li>Validación por ingreso de nombre vacío</li>
+<li>Validación nombre existente (evita duplicados)</li>
+<li>Visualización nombres ingresados, ordenados alfabéticamente</li>
+<li>Selección de un ganador de la lista de amigos</li>
+<li>Validación mínimo 2 amigos para realizar sorteo</li>
+<li>Borrar lista para ingresar nuevos nombres</li>
+<h2>Acceso al Proyecto</h2>
+<h2>Tecnologías Utilizadas</h2>
+<li>Html 5</li>
+<li>Javascript</li>
+<li>Css</li>
+<h2>Colaboradores del Proyecto</h2>
+<li>Alura Latam</li>
+<h2>Desarrolladores del Proyecto</h2>
+<li>Engelberth Toledo Meyer</li>
--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//Agreaga amigos al array
+function agregarAmigo(){
+    let amigoInput = document.getElementById("amigo").value;
+
+    console.log(amigoInput);
+
+    if(amigoInput.length > 0){
+        amigos.includes(amigoInput.toUpperCase()) ? alert("Amigo registrado previamente en la lista") : amigos.push(amigoInput.toUpperCase());
+        amigos.sort();
+        document.querySelector('#amigo').value = "";
+        console.log(amigos);
+        
+        listarAmigos();
+    }
+    else alert("Por favor, ingrese un nombre");
+}
+
+//Agrega amigo a la lista HTML
+function listarAmigos(){
+    let listaHtml = document.getElementById('listaAmigos');
+    let elemento = listaHtml.getElementsByTagName('li');
+
+    console.log(`Elemento:${elemento}`);
+
+    /*Borra lista html*/
+    if (elemento.length > 0){
+        listaHtml.innerHTML = "";        
+    }
+    /*Despliega nueva lista html*/
+    for(let i = 0; i < amigos.length; i++){
+        console.log(`i:${i}, ${amigos[i]}`)
+        let li = document.createElement('li');
+        li.innerText = amigos[i];
+        listaHtml.appendChild(li);
+    }
+}
+
+//Determina un ganador
+function sortearAmigo(){
+    let ganador = document.getElementById('resultado');
+    let numeroAmigos = amigos.length;
+    let numeroGanador;
+
+    if (numeroAmigos < 2){
+        alert("Debe ingresar mínimo 2 amigos para poder sortear");
+    }
+    else{
+        numeroGanador = Math.floor(Math.random()*(numeroAmigos));
+
+        console.log(`Index: ${numeroGanador} - Amigo: ${amigos[numeroGanador]}`);
+
+        ganador.innerHTML = amigos[numeroGanador];
+    }
+}
+
+//Limpia el formulario
+function borrarSorteo(){
+    document.getElementById('listaAmigos').innerHTML="";
+    document.getElementById('resultado').innerHTML = "";
+    amigos=[];
+    console.log(`Lista amigos:${amigos.length}`)
+}
